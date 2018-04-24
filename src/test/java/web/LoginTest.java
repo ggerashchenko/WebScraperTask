8f820d811bf9c4e8e964375304e77f8d74651f1b
Grygorii Gerashchenko <grygorii.gerashchenko@veon.com>
2018-04-24 21:39:28 +0200

    Updated tests to cleaner look

--- src/test/java/web/LoginTest.java
+++ src/test/java/web/LoginTest.java
@@ -3,13 +3,11 @@ package web;
 import com.codeborne.selenide.Condition;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import pages.ErrorLoginPage;
 import pages.LoginPage;
 import pages.SuccessLoginPage;
 
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class LoginTest extends BasicTestCase {
@@ -19,9 +17,8 @@ public class LoginTest extends BasicTestCase {
 	@Test
 	@DisplayName("Test success login to the system.")
 	public void successLoginTest() {
-		loginPage.successLogin("admin", "12345");
-		$(By.className("success")).isDisplayed();
-		$(By.className("success")).shouldHave(Condition.exactText("WELCOME :)"));
+		SuccessLoginPage successLoginPage = loginPage.successLogin("admin", "12345");
+		verifyMessageDisplayed(successLoginPage.successLoginMessage, "WELCOME :)");
 	}
 
 	@Test
@@ -32,24 +29,21 @@ public class LoginTest extends BasicTestCase {
 		driver.manage().deleteCookieNamed("tdsess");
 		driver.navigate().refresh();
 		ErrorLoginPage errorLoginPage = new ErrorLoginPage();
-		errorLoginPage.sessionMissingCookieError.isDisplayed();
-		errorLoginPage.sessionMissingCookieError.shouldHave(Condition.exactText("THE SESSION COOKIE IS MISSING OR HAS A WRONG VALUE!"));
+		verifyMessageDisplayed(errorLoginPage.sessionMissingCookieError, "THE SESSION COOKIE IS MISSING OR HAS A WRONG VALUE!");
 	}
 
 	@Test
 	@DisplayName("Test access denied status without credentials.")
 	public void accessDeniedWithoutCredentialsForLoginTest() {
 		ErrorLoginPage errorLoginPage = loginPage.failedLogin();
-		errorLoginPage.accessDeniedError.isDisplayed();
-		errorLoginPage.accessDeniedError.shouldHave(Condition.exactText("ACCESS DENIED!"));
+		verifyMessageDisplayed(errorLoginPage.accessDeniedError, "ACCESS DENIED!");
 	}
 
 	@Test
 	@DisplayName("Test access denied status with wrong credentials.")
 	public void accessDeniedWithWrongCredentialsForLoginTest() {
 		ErrorLoginPage errorLoginPage = loginPage.failedLogin("incorrectUserName", "wrongPassword");
-		errorLoginPage.accessDeniedError.isDisplayed();
-		errorLoginPage.accessDeniedError.shouldHave(Condition.exactText("ACCESS DENIED!"));
+		verifyMessageDisplayed(errorLoginPage.accessDeniedError, "ACCESS DENIED!");
 	}
 
 	@Test
